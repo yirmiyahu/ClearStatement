cae7c5500423b771666987f65c54f66023ca7e5c
Jeremy Tan <emailjeremytan@gmail.com>
2019-02-08 16:35:14 -0800

    Add lit-html as dependency

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
+// interface SupportClaimsMap {
+//   [_: string]: HTMLElement;
+// }
+
+// const ATTRIBUTE_SUPPORT = 'for';
+
 export default class ClearStatement extends HTMLElement {
   static is = 'clear-statement';
+  // private supportClaimsMap: SupportClaimsMap;
+
+  // constructor() {
+  //   super();
+  //   const shadowRoot = this.attachShadow({ mode: 'open' });
+  //   const template = document.getElementById('clear-statement') as HTMLTemplateElement;
+  //   shadowRoot.appendChild(template.content.cloneNode(true));
+  //   this.supportClaimsMap = this.mapSupports();
+  // }
+
+  // mapSupports(): SupportClaimsMap {
+  //   const supportsSlot = this.shadowRoot.getElementById('support') as HTMLSlotElement;
+  //   const supportNodes = supportsSlot.assignedNodes();
+
+  //   if (!supportNodes.length) {
+  //     return;
+  //   }
+
+  //   return Array.prototype.reduce.call(supportNodes, (store: SupportClaimsMap, support: HTMLElement) => {
+  //     store[support.getAttribute(ATTRIBUTE_SUPPORT)] = support;
+  //     return store;
+  //   }, {});
+  // }
 }
